b0d916d3e3310302c208183f52f2ccd8e71c7147
Adam Koziol <adam.koziol@inspection.gc.ca>
2024-04-16 19:50:54 +0000

    refactor: improve readability and remove commented lines in setup.py

--- setup.py
+++ setup.py
@@ -12,20 +12,18 @@ from setuptools import setup, find_packages
 
 # Find the version
 version = {}
-with open(convert_path(os.path.join('allele_tools', 'version.py')), 'r') as version_file:
+with open(convert_path(
+        os.path.join('allele_tools', 'version.py')), 'r') as version_file:
     exec(version_file.read(), version)
 
 setup(
     name="AlleleFinder",
     version=version['__version__'],
     scripts=[
-        # os.path.join('allele_tools', 'allele_finder.py'),
         os.path.join('allele_tools', 'allele_profiler.py'),
         os.path.join('allele_tools', 'allele_updater.py'),
         os.path.join('allele_tools', 'allele_translate_reduce.py'),
-        # os.path.join('allele_tools', 'probe_creator.py'),
         os.path.join('allele_tools', 'profile_reduce.py'),
-        # os.path.join('allele_tools', 'stec_attributer.py'),
         os.path.join('allele_tools', 'stec.py')
     ],
     packages=find_packages(),
