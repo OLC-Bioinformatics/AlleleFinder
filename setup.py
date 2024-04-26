2e0d839f813d488f4e686570d6ca9a2178712d7b
Adam Koziol <adam.koziol@inspection.gc.ca>
2024-04-26 15:12:38 +0000

    feat: enhance setup.py
    - Add code to read README.md for long description
    
    - Add code to read requirements from requirements.txt
    
    - Add install_requires to setup function
    
    - Add classifiers to setup function

--- setup.py
+++ setup.py
@@ -16,6 +16,16 @@ with open(convert_path(
         os.path.join('allele_tools', 'version.py')), 'r') as version_file:
     exec(version_file.read(), version)
 
+# Read the contents of your README file
+with open('README.md', 'r') as f:
+    long_description = f.read()
+    
+# Open the requirements.txt file
+with open('requirements.txt') as f:
+    # Read the file and split it into lines
+    # Each line should be a separate requirement
+    requirements = f.read().splitlines()
+
 setup(
     name="AlleleFinder",
     version=version['__version__'],
@@ -31,4 +41,19 @@ setup(
     author="Adam Koziol",
     author_email="adam.koziol@inspection.gc.ca",
     url="https://github.com/OLC-Bioinformatics/AlleleFinder",
+    # The requirements from requirements.txt
+    install_requires=requirements,
+    # Classifiers help users find your project by categorizing it.
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+        # Specify the Python versions you support here
+        'Programming Language :: Python :: 3.9',
+        # Show your project's status - planning, alpha, beta, or stable
+        'Development Status :: 5 - Production/Stable',
+        # Indicate if your project relates to a particular topic
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
 )
